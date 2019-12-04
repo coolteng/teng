b127ff1cfe1f15a37ee95ad7e240a2c7129ad05e
鄧崇林(Chung-Lin Teng) <coolteng@gmail.com>
2019-12-04 16:02:03 +0800

    Update right_box.js

--- carouselMenu/right_box.js
+++ carouselMenu/right_box.js
@@ -12,7 +12,8 @@ function closea(){
 	div1.style.cssText="display:none;";
 }
 var link = 'https://xw.qq.com/cmsid/20190920A008XL00';
-var title = '2019 鄧崇林的創新入口網站非常歡迎並邀您参加👍';
+//var title = '2019 鄧崇林的創新入口網站非常歡迎並邀您参加👍';
+var title = '2019 發表論文：以量纲分析重新发现普朗克公式👍';
 var tp = cltYYMMDD + '  試讀';
 div1.innerHTML = "<div style=\"text-align:right; font:14px/24px Arial, Helvetica, sans-serif, '宋體'; width:30px; text-align:center; float:right;\"><a onclick=\"closea()\" style=\"color:#ffffff;cursor:pointer;\">×</a></div><div style=\"padding:10px 0px;font-size: 14px;line-height: 20px; background: #222222;\"><a href=\""+link+"\" target=\"_blank\" style=\"display: block;text-decoration:none;\" ><p style=\"width: 164px;padding: 0px 8px 0px 10px;color: #ffffff;text-align: left;\"><span style=\"color: #c00;background: #c00;color: #fff;border-radius: 3px;font-size: 12px;display: inline-block;padding: 0px 5px;line-height: 18px;margin-bottom: 6px;\">"+tp+"</span><br />"+title+"</p></a></div>";
 div1.style.cssText="position: fixed;bottom: 450px;right: 0;width: 180px;z-index: 9999;"
